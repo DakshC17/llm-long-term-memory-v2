fbe4ba47682f2b86c68475e3ec3b3a1f403dc134
daksh <dakshc1709@gmail.com>
2025-09-06 23:57:16 +0530

    initialised the class

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,3 +4,17 @@ import sys
 from pathlib import Path
 
 #imported the required files that will be required for cli testing..
+
+
+
+class TestBrunoCLI(unittest.TestCase):
+    def test_cli_help(self):
+        """IT will Test that CLI help runs without error"""
+        result = subprocess.run(
+            [sys.executable, str(Path("src/bruno_cli.py")), "--help"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+
+        ##class will check the help function for cli tetsing
