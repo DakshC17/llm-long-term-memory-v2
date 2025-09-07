ab518e7f11bc3fd3c8d63f57c8271537aad7c44d
daksh <dakshc1709@gmail.com>
2025-09-08 00:23:52 +0530

    combined test src added

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -5,3 +5,11 @@ from pathlib import Path
 #added the import files that will be required for the final testing ..
 #in this testing we will be doing the combine cli as well as agent testing
 #next we will do the same thing by adding the src
+
+
+# Add src to path
+sys.path.append(str(Path(__file__).parent.parent / "src"))
+from agent import MemoryEnhancedAgent
+
+# here we will be adding the src path for the memory agent 
+# for the memory agent 
