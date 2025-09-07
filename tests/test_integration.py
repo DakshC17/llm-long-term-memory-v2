a11eca685f187bddca170efaad3d8154bde83bae
daksh <dakshc1709@gmail.com>
2025-09-08 00:20:43 +0530

    Combined testing

---
+++ tests/test_integration.py
@@ -0,0 +1,7 @@
+import unittest
+import sys
+from pathlib import Path
+
+#added the import files that will be required for the final testing ..
+#in this testing we will be doing the combine cli as well as agent testing
+#next we will do the same thing by adding the src
